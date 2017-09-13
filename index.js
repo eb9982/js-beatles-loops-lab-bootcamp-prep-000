f5e00df2c0e26055609ecfd21f47894008510c97
Luke Borsare <lborsare1@gmail.com>
2017-09-13 00:52:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments) {
+  var array = []
   for (var i = 0; i < 4; i++) {
-    var array = []
     array.push(musicians[i] + ' plays ' + (instruments[i]))
     return array
   }
